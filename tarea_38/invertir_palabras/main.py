417f6e6a5154b19535f5c469868a74f33eb9b8aa
alafa <ainhoalafuente011@gmail.com>
2020-10-24 11:07:50 +0200

    arreglar codigo de invertir_palabras (tarea38)

--- tarea_38/invertir_palabras/main.py
+++ tarea_38/invertir_palabras/main.py
@@ -4,15 +4,29 @@ import sys
 if __name__ == "__main__":
 
     if len(sys.argv) < 2:
-        print('ERROR: Tienes que proporcionar la frase')
-        exit()
-    elif len(sys.argv) > 2:
-        print("ERROR: Asegurate de envolver la frase entre comillas dobles.")
+        print('ERROR: Tienes que proporcionar el número de frases que se quiere invertir')
         exit()
 
-    words = sys.argv[1].split(' ')
-    inverse_sentence = []
-    for i in range(len(words) - 1, -1, -1):
-        inverse_sentence.append(words[i])
+    try:
+        num_sentences = int(sys.argv[1])
+    except:
+        print('ERROR: El argumento debe ser un número entero')
+        exit()
 
-    print(' '.join(inverse_sentence))
+    results = []
+
+    for i in range(0, num_sentences):
+
+        print('')
+        print(f"#{i+1} Escribe una frase y presiona enter:")
+        sentence_input = input()
+        words = sentence_input.split(' ')
+        inverse_sentence = []
+        for i in range(len(words) - 1, -1, -1):
+            inverse_sentence.append(words[i])
+
+        results.append(' '.join(inverse_sentence))
+
+    print("\nFRASES INVERTIDAS:")
+    for i, result in enumerate(results):
+        print(f"#Case {i+1}: {result}")
