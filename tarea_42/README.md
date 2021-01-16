31faa516753118adf161cd6d040953b4db1456d1
alafa <ainhoalafuente011@gmail.com>
2021-01-16 18:45:01 +0100

    preparar video item model

--- tarea_42/README.md
+++ tarea_42/README.md
@@ -1,3 +1,7 @@
 # EN DESAROLLO
 
-Se necesita instalar Django.
+Se necesita instalar Django y Pillow.
+
+`python -m pip install django`
+
+`python -m pip install Pillow`
--- tarea_42/collection/models.py
+++ tarea_42/collection/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class VideoItem(models.Model):
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.IntegerField(
+        choices=(
+            (0, 'Música'),
+            (1, 'Cine'),
+            (2, 'Animales'),
+            (3, 'Ciencia'),
+            (4, 'Otros')
+        ),
+        default=0
+    )
+    image_preview = models.ImageField(upload_to='video_collection/uploads/', default=None)
+    url = models.CharField(max_length=100)
